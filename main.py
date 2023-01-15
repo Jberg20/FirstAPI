5788e0ae4f8aecfe61270bf334f5dcb6f2eab8bd
Jberg20 <jackberger20@gmail.com>
2023-01-15 12:35:26 -0700

    Basic HelloWorld get function

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 from Flask import Flask
-#hello!
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"Hello World"}
+
+api.add_resource(HelloWorld, "/helloworld")
+
+if __name__ == "__main__":
+    app.run(debug=True)
