efad0bdf3078eebc6ba51327f6ff722b55c99826
Jberg20 <jackberger20@gmail.com>
2023-01-15 17:45:59 -0700

    seperated band and bandlist. a Bandlst GET will return the whole database. Band GET wil just return the band based on the ID, band POST wil add a band to the database. For some reason POST is returning a 405.

--- main.py
+++ main.py
@@ -1,20 +1,60 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
+import sqlite3
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {}
+class BandList(Resource):
+    def get(self):
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM bands")
+        result = c.fetchall()
+        if result:
+            return {"message": result}
+        else:
+            return {"message": "No bands found"}
 
-class HelloWorld(Resource):
-    def get(self, name, test):
-        return {"name": name, "test": test}
-    
-    def post(self, name, test):
-        return {"name": name, "test": test}
+class Band(Resource):
+    def get(self, band_id):
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute("SELECT * FROM bands WHERE band_id=?", (band_id,))
+        result = c.fetchone()
+        if result:
+            return {"message": result}
+        else:
+            return {"message": "Band not found"}
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>/<int:test>")
+    def post(self):
+        data = request.get_json()
+        if not all(key in data for key in ('band_name', 'band_genre', 'gigs', 'rating')):
+            return {"message": "Missing data"}
+        band_name = data['band_name']
+        band_genre = data['band_genre']
+        gigs = data['gigs']
+        rating = data['rating']
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO bands (band_name,band_genre,gigs,rating) VALUES (?,?,?,?)", (band_name,band_genre,gigs,rating))
+        conn.commit()
+        return {"message": "Success"}
+
+api.add_resource(BandList, '/bands')
+api.add_resource(Band, '/bands/<int:band_id>')
+
 
 if __name__ == "__main__":
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("DROP TABLE IF EXISTS bands")
+    c.execute("""CREATE TABLE IF NOT EXISTS bands (
+                band_id INTEGER PRIMARY KEY,
+                band_name TEXT,
+                band_genre TEXT,
+                gigs INTEGER,
+                rating INTEGER
+                )""")
+    conn.commit()
     app.run(debug=True)
-    
--- test.py
+++ test.py
@@ -2,8 +2,9 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
+response = requests.post(BASE + "helloworld/Jack/22")
+print(response.json())
+
 response = requests.get(BASE + "helloworld/Jack/22")
 print(response.json())
 
-response = requests.post(BASE + "helloworld/Jack/22")
-print(response.json())
