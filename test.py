ce000786b8c4f861c8696bcf5d13fb7a0cfa5802
Jberg20 <jackberger20@gmail.com>
2023-01-15 17:52:35 -0700

    updated test.py, looks like the post method just doesn't work at all, gives me a 405. Can't tell you why at all lmao

--- test.py
+++ test.py
@@ -1,10 +1,28 @@
 import requests
 
-BASE = "http://127.0.0.1:5000/"
+#BASE = "http://127.0.0.1:5000/"
 
-response = requests.post(BASE + "helloworld/Jack/22")
-print(response.json())
+data = {
+    "band_name": "ACDC",
+    "band_genre": "Rock",
+    "gigs": 100,
+    "rating": 4
+}
 
-response = requests.get(BASE + "helloworld/Jack/22")
-print(response.json())
+response = requests.post("http://127.0.0.1:5000/bands", json=data)
+
+if response.status_code == 201:
+    print("201!")
+    print(response.json())
+else:
+    print("Error:", response.status_code)
+
+response = requests.get("http://127.0.0.1:5000/bands/1")
+
+if response.status_code == 200:
+    print("200!")
+    print(response.json())
+else:
+    print("Error:", response.status_code)
+
 
