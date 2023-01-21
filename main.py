92a466f40db77ad3f394022378f21f736c5816aa
Jberg20 <jackberger20@gmail.com>
2023-01-21 14:28:45 -0700

    GOT IT WORKING POGgit add .

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from flask import Flask, request, jsonify
-from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
-import sqlite3
 
 app = Flask(__name__)
-api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 db = SQLAlchemy(app)
-
-
-
 
 class Band(db.Model):
     band_ID = db.Column(db.Integer, primary_key=True)
@@ -25,6 +20,10 @@ class Band(db.Model):
                 'band_Gigs': self.band_Gigs,
                 'band_Rating': self.band_Rating
         }
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
 
 @app.route('/bands/<int:band_ID>', methods=['GET'])
 def get_band(band_ID):
@@ -34,9 +33,21 @@ def get_band(band_ID):
     else:
         return jsonify({"message": "Band not found"}), 404
 
-    
+@app.route('/bands', methods=['POST'])
+def make_band():
+    data = request.get_json()
+    if not all(key in data for key in ('band_Name', 'band_Genre', 'band_Gigs', 'band_Rating')):
+            return {"message": "Missing data"}, 400
+    band = Band(band_Name=data['band_Name'], band_Genre=data['band_Genre'], band_Gigs=data['band_Gigs'], band_Rating=data['band_Rating'])
+    db.session.add(band)
+    db.session.commit()
+    return {"message": "Success"}, 201
 
 
 if __name__ == "__main__":
-    db.create_all()
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    db.init_app(app)
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+
